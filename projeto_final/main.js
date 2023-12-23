63ebb0a8549d220f8d6d613c9254e1ba81d1805a
kssiodm <cassiodemaz@gmail.com>
2023-12-23 15:17:33 -0300

    update

--- projeto_final/main.js
+++ projeto_final/main.js
@@ -5,13 +5,13 @@ document.getElementById('searchForm').addEventListener('submit', function (e) {
     const apiKey = 'e684ab1ca25ce9861ccd1c17032e82e6';
 
     // Obtenha o valor do campo de pesquisa
-    const movieTitle = document.getElementById('movieTitle').value;
+    const searchTerm = document.getElementById('movieTitle').value;
 
     // Especifique o idioma desejado (português do Brasil)
     const language = 'pt-BR';
 
     // Construa a URL da API TMDb incluindo o parâmetro de idioma
-    const apiUrl = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&query=${movieTitle}&language=${language}`;
+    const apiUrl = `https://api.themoviedb.org/3/search/multi?api_key=${apiKey}&query=${searchTerm}&language=${language}`;
 
     // Faça a solicitação à API usando fetch
     fetch(apiUrl)
@@ -20,7 +20,7 @@ document.getElementById('searchForm').addEventListener('submit', function (e) {
         // Exiba os resultados na página
         displayResults(data.results);
         })
-    .catch(error => console.error('Erro ao fazer a solicitação à API:', error));
+        .catch(error => console.error('Erro ao fazer a solicitação à API:', error));
 });
 
   // Função para exibir os resultados na página
@@ -37,17 +37,35 @@ function displayResults(results) {
     }
 
     // Crie elementos para cada resultado e adicione à página
-    results.forEach(movie => {
-        const movieElement = document.createElement('div');
-        movieElement.classList.add('movie');
+    results.forEach(item => {
+        const resultElement = document.createElement('div');
+        resultElement.classList.add('result');
+    
+        let title, releaseDate;
+        
+        if (item.media_type === 'movie') {
+            title = item.title;
+            releaseDate = item.release_date;
+        } else if (item.media_type === 'tv') {
+            title = item.name;
+            releaseDate = item.first_air_date;
+        } else {
+            // Se não for um filme ou série, pule este resultado
+            return;
+        }
 
-        movieElement.innerHTML = `
-        <h2>${movie.title}</h2>
-        <p>${movie.overview}</p>
-        <p>Data de Lançamento: ${movie.release_date}</p>
-        <p>Avaliação Média: ${movie.vote_average}</p>
-        `;
+    const imageUrl = item.poster_path
+        ? `https://image.tmdb.org/t/p/w500${item.poster_path}`
+        : 'placeholder-image-url.jpg'; // URL de uma imagem de espaço reservado, se não houver imagem disponível
 
-    resultsContainer.appendChild(movieElement);
+    resultElement.innerHTML = `
+        <h2>${title}</h2>
+        <img src="${imageUrl}" alt="${title} Poster">
+        <p>${item.overview}</p>
+        <p>Data de Lançamento: ${releaseDate}</p>
+        <p>Avaliação Média: ${item.vote_average}</p>
+    `;
+
+    resultsContainer.appendChild(resultElement);
     });
 }
